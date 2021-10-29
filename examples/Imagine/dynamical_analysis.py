c1237185f2582687374eef9f350ff2274b62fc49
smbct <smbct.dev@yahoo.com>
2021-10-29 19:19:28 +0200

    expe transitions parameters

--- examples/Imagine/dynamical_analysis.py
+++ examples/Imagine/dynamical_analysis.py
@@ -10,6 +10,8 @@ from matplotlib import colors as mcolors
 import matplotlib.ticker as ticker
 from matplotlib import cm
 
+import os
+
 import sys
 sys.path.append('../../python')
 
@@ -42,17 +44,20 @@ def read_quality_file(filename):
 #-------------------------------------------------------------------------------
 def plot_quality(data, ax):
 
-    ax.set_xlabel('Quality')
-    ax.set_ylabel(r'Proportion between $\mathcal{S}^+$ and $\mathcal{S}^-$')
+    # ax.set_xlabel('Quality')
+    # ax.set_ylabel(r'Proportion between $\mathcal{S}^+$ and $\mathcal{S}^-$')
 
     indexes = [ind for ind in range(len(data)) if data[ind][2] > 0]
 
     qualities = [data[ind][2] for ind in indexes]
     proportions = [data[ind][3]/(data[ind][3]+data[ind][4]) for ind in indexes]
 
-    colors = ['red' if data[ind][1] == 0 else 'blue' for ind in indexes]
+    colors = ['firebrick' if data[ind][1] == 0 else 'royalblue' for ind in indexes]
 
-    ax.scatter(qualities, proportions, s=2, marker='o', c=colors)
+    ax.scatter(qualities, proportions, s=0.6, marker='.', c=colors)
+
+    ax.set_xlim((0.5, 1.))
+    ax.set_ylim((0., 1.))
 
     # ind2 = 0
     # for ind in indexes:
@@ -64,32 +69,104 @@ def plot_quality(data, ax):
 
     return
 
-print('hello dynamics')
+#-------------------------------------------------------------------------------
+def extract_files_parameters(files):
 
-# filename = 'coexpression_quality.txt'
-# # filename = 'transitions_quality.txt'
-# data = read_quality_file(filename)
-# fig, ax = plt.subplots()
-# plot_quality(data, ax)
-# plt.show()
+    # select files from the experiment (.txt)
+    expe_files = [file for file in files if file[-3:] == 'txt']
 
-filename = '../../dataset/Imagine/transitions.csv'
-df = pd.read_csv(filename, index_col=0)
-print(df.head())
+    result = []
 
-successors = {}
+    # extract parameters
+    for file in expe_files:
+        tokens = file[:-4].split('_')
+        parameters = (float(tokens[3]), int(tokens[5]), int(tokens[7]))
+        result.append((file, parameters))
 
-for index in df.index:
-    left = df['T-1'][index]
-    right = df['T'][index]
+    return result
 
-    if left in successors:
-        successors[left].append(right)
-    else:
-        successors[left] = [right]
+#-------------------------------------------------------------------------------
+def plot_tau(subfig, rho, delta, files_parameters):
 
-print(successors)
+    nrows = 3
+    ncols = 2
 
-nsuc = [len(successors[elt]) for elt in successors]
-nsuc.sort()
-print(nsuc)
+    axs = subfig.subplots(nrows, ncols)
+    axs = axs.flat
+    # fig.tight_layout()
+    subfig.delaxes(axs[-1])
+
+    subfig.suptitle('$\\rho = ' + str(rho) + ', \\delta = ' + str(delta) + '$' )
+
+    selected_param = [elt for elt in files_parameters if elt[1][1] == rho and elt[1][2] == delta]
+    selected_param.sort(key=lambda elt: elt[1][0])
+    # print(selected_param)
+
+    index = 0
+    for elt in selected_param:
+
+        data = read_quality_file(path + '/' + elt[0])
+        plot_quality(data, axs[index])
+
+        # title = '$\\tau = ' + str(elt[1][0]) + ', \\rho = ' + str(elt[1][1]) + ', \\delta = ' + str(elt[1][2]) + '$'
+        title = '$\\tau = ' + str(elt[1][0]) + '$'
+
+        # print(title)
+        axs[index].set_title(title)
+
+        axs[index].xaxis.set_major_locator(ticker.NullLocator())
+        axs[index].yaxis.set_major_locator(ticker.NullLocator())
+
+        index += 1
+
+    return
+
+
+# list all files in a folder
+
+path = '../../dataset/Imagine/transitions_parameters_expe'
+files = os.listdir(path)
+
+files_parameters = extract_files_parameters(files)
+
+
+
+fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+subfigs = fig.subfigures(1, 2, wspace=0.15)
+
+rho = 0
+delta = 1
+plot_tau(subfigs[0], rho, delta, files_parameters)
+
+rho = 1
+delta = 1
+plot_tau(subfigs[1], rho, delta, files_parameters)
+
+#################################################################
+
+fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+subfigs = fig.subfigures(1, 2, wspace=0.15)
+
+rho = 2
+delta = 1
+plot_tau(subfigs[0], rho, delta, files_parameters)
+
+rho = 0
+delta = 2
+plot_tau(subfigs[1], rho, delta, files_parameters)
+
+#####################################################################
+
+fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+subfigs = fig.subfigures(1, 2, wspace=0.15)
+
+rho = 1
+delta = 2
+plot_tau(subfigs[0], rho, delta, files_parameters)
+
+rho = 2
+delta = 2
+plot_tau(subfigs[1], rho, delta, files_parameters)
+
+
+plt.show()
