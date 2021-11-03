c16c135f4fe11c1e0790d099d7ea5ae920613aee
smbct <buchet.s@laposte.net>
2021-11-03 19:17:28 +0100

    code quality graphs

--- examples/Imagine/dynamical_analysis.py
+++ examples/Imagine/dynamical_analysis.py
@@ -20,14 +20,15 @@ from instance import Instance
 import visualizer
 import histogram
 
-# 0: gene name
-# 1: gene discrete value
-# 2: quality
-# 3 : number of positive examples
-# 4 : number of negative examples
 
 #-------------------------------------------------------------------------------
 def read_quality_file(filename):
+
+    # 0: gene name
+    # 1: gene discrete value
+    # 2: quality
+    # 3 : number of positive examples
+    # 4 : number of negative examples
 
     file = open(filename, 'r')
     content = file.read().splitlines()
@@ -42,22 +43,30 @@ def read_quality_file(filename):
     return data
 
 #-------------------------------------------------------------------------------
-def plot_quality(data, ax):
-
-    # ax.set_xlabel('Quality')
-    # ax.set_ylabel(r'Proportion between $\mathcal{S}^+$ and $\mathcal{S}^-$')
+def plot_quality(data, ax, annotations):
 
     indexes = [ind for ind in range(len(data)) if data[ind][2] > 0]
 
-    qualities = [data[ind][2] for ind in indexes]
-    proportions = [data[ind][3]/(data[ind][3]+data[ind][4]) for ind in indexes]
+    if not annotations:
+        qualities = [data[ind][2] for ind in indexes]
+        proportions = [data[ind][3]/(data[ind][3]+data[ind][4]) for ind in indexes]
+        colors = ['firebrick' if data[ind][1] == 0 else 'royalblue' for ind in indexes]
+        ax.scatter(qualities, proportions, s=0.6, marker='.', c=colors)
+        # ax.scatter(qualities, proportions, s=6, marker='.', c=colors)
+    else:
+        qualities_pos = [data[ind][2] for ind in indexes if data[ind][1] > 0]
+        proportions_pos = [data[ind][3]/(data[ind][3]+data[ind][4]) for ind in indexes if data[ind][1] > 0]
+        qualities_nul = [data[ind][2] for ind in indexes if data[ind][1] == 0]
+        proportions_nul = [data[ind][3]/(data[ind][3]+data[ind][4]) for ind in indexes if data[ind][1] == 0]
+        ax.scatter(qualities_nul, proportions_nul, s=6, marker='.', c='firebrick', label='atomes nuls')
+        ax.scatter(qualities_pos, proportions_pos, s=6, marker='.', c='royalblue', label='atomes non nuls')
 
-    colors = ['firebrick' if data[ind][1] == 0 else 'royalblue' for ind in indexes]
 
-    ax.scatter(qualities, proportions, s=0.6, marker='.', c=colors)
+        ax.set_xlabel('qualité des corrélations')
 
-    ax.set_xlim((0.5, 1.))
-    ax.set_ylim((0., 1.))
+
+        ax.set_ylabel(r'proportion entre $\mathcal{S}^+$ et $\mathcal{S}^-$')
+        ax.legend(loc='lower left', markerscale=4.0)
 
     # ind2 = 0
     # for ind in indexes:
@@ -65,7 +74,15 @@ def plot_quality(data, ax):
     #         ax.text(qualities[ind2], proportions[ind2], data[ind][0] + '_' + str(data[ind][1])).set_clip_on(True)
     #     ind2 += 1
 
+
+
+
+    ax.set_xlim((0.5, 1.))
+    ax.set_ylim((0., 1.))
+
     ax.set_aspect((ax.get_xlim()[1]-ax.get_xlim()[0]) / (ax.get_ylim()[1]-ax.get_ylim()[0]))
+
+
 
     return
 
@@ -86,15 +103,24 @@ def extract_files_parameters(files):
     return result
 
 #-------------------------------------------------------------------------------
-def plot_tau(subfig, rho, delta, files_parameters):
+def plot_tau(subfig, rho, delta, files_parameters, config = 0, labels = 0):
 
-    nrows = 3
-    ncols = 2
+    path = '../../dataset/Imagine/transitions_parameters_expe'
+
+    if config == 0:
+        nrows = 3
+        ncols = 2
+    else:
+        nrows = 5
+        ncols = 1
 
     axs = subfig.subplots(nrows, ncols)
     axs = axs.flat
+
     # fig.tight_layout()
-    subfig.delaxes(axs[-1])
+
+    if config == 0:
+        subfig.delaxes(axs[-1])
 
     subfig.suptitle('$\\rho = ' + str(rho) + ', \\delta = ' + str(delta) + '$' )
 
@@ -106,67 +132,122 @@ def plot_tau(subfig, rho, delta, files_parameters):
     for elt in selected_param:
 
         data = read_quality_file(path + '/' + elt[0])
-        plot_quality(data, axs[index])
+        plot_quality(data, axs[index], False)
 
         # title = '$\\tau = ' + str(elt[1][0]) + ', \\rho = ' + str(elt[1][1]) + ', \\delta = ' + str(elt[1][2]) + '$'
-        title = '$\\tau = ' + str(elt[1][0]) + '$'
 
         # print(title)
-        axs[index].set_title(title)
+
+        if config == 0:
+            if elt[1][0] < 0:
+                title = '$\\tau = \epsilon$'
+            else:
+                title = '$\\tau = ' + str(elt[1][0]) + '$'
+
+            axs[index].set_title(title)
 
         axs[index].xaxis.set_major_locator(ticker.NullLocator())
         axs[index].yaxis.set_major_locator(ticker.NullLocator())
+
+        # if labels == 1:
+        #     axs[index].set_ylabel('$\\tau = ' + str(elt[1][0]) + '$', rotation=0, size='large', labelpad=30.0)
+
 
         index += 1
 
     return
 
 
-# list all files in a folder
+#-------------------------------------------------------------------------------
+def plot_transitions_parameters_quality():
 
-path = '../../dataset/Imagine/transitions_parameters_expe'
-files = os.listdir(path)
+    # list all files in a folder
 
-files_parameters = extract_files_parameters(files)
+    path = '../../dataset/Imagine/transitions_parameters_expe'
+    files = os.listdir(path)
 
-
-
-fig = plt.figure(constrained_layout=True, figsize=(10, 4))
-subfigs = fig.subfigures(1, 2, wspace=0.15)
-
-rho = 0
-delta = 1
-plot_tau(subfigs[0], rho, delta, files_parameters)
-
-rho = 1
-delta = 1
-plot_tau(subfigs[1], rho, delta, files_parameters)
-
-#################################################################
-
-fig = plt.figure(constrained_layout=True, figsize=(10, 4))
-subfigs = fig.subfigures(1, 2, wspace=0.15)
-
-rho = 2
-delta = 1
-plot_tau(subfigs[0], rho, delta, files_parameters)
-
-rho = 0
-delta = 2
-plot_tau(subfigs[1], rho, delta, files_parameters)
-
-#####################################################################
-
-fig = plt.figure(constrained_layout=True, figsize=(10, 4))
-subfigs = fig.subfigures(1, 2, wspace=0.15)
-
-rho = 1
-delta = 2
-plot_tau(subfigs[0], rho, delta, files_parameters)
-
-rho = 2
-delta = 2
-plot_tau(subfigs[1], rho, delta, files_parameters)
+    files_parameters = extract_files_parameters(files)
 
 
-plt.show()
+
+    fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+    subfigs = fig.subfigures(1, 2, wspace=0.15)
+
+    rho = 0
+    delta = 1
+    plot_tau(subfigs[0], rho, delta, files_parameters, 0, 0)
+
+    rho = 1
+    delta = 1
+    plot_tau(subfigs[1], rho, delta, files_parameters, 0, 0)
+
+    #################################################################
+
+    # fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+    # subfigs = fig.subfigures(1, 2, wspace=0.15)
+    #
+    # rho = 2
+    # delta = 1
+    # plot_tau(subfigs[0], rho, delta, files_parameters)
+    #
+    # rho = 0
+    # delta = 2
+    # plot_tau(subfigs[1], rho, delta, files_parameters)
+    #
+    # #####################################################################
+    #
+    # fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+    # subfigs = fig.subfigures(1, 2, wspace=0.15)
+    #
+    # rho = 1
+    # delta = 2
+    # plot_tau(subfigs[0], rho, delta, files_parameters)
+    #
+    # rho = 2
+    # delta = 2
+    # plot_tau(subfigs[1], rho, delta, files_parameters)
+
+    #####################################################################
+    fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+    # subfigs = fig.subfigures(1, 4, wspace=0.15)
+    subfigs = fig.subfigures(1, 5, wspace=0.05)
+
+    subfigs[0].suptitle(' ')
+    axs = subfigs[0].subplots(5, 1)
+    axs = axs.flat
+
+    ind = 0
+    for tau in ['\\epsilon', '0.2', '0.4', '0.6', '0.8']:
+        axs[ind].text(0.4, 0.5, '$\\tau = ' + tau + '$', size='large', horizontalalignment='left', verticalalignment='center')
+        axs[ind].axis('off')
+        ind += 1
+
+    rho = 2
+    delta = 1
+    plot_tau(subfigs[1], rho, delta, files_parameters, 1, 1)
+
+    rho = 0
+    delta = 2
+    plot_tau(subfigs[2], rho, delta, files_parameters, 1, 0)
+
+    rho = 1
+    delta = 2
+    plot_tau(subfigs[3], rho, delta, files_parameters, 1, 0)
+
+    rho = 2
+    delta = 2
+    plot_tau(subfigs[4], rho, delta, files_parameters, 1, 0)
+
+    plt.show()
+
+    return
+
+plot_transitions_parameters_quality()
+
+# filename = 'coexpression_quality.txt'
+# data = read_quality_file(filename)
+# fig, ax = plt.subplots()
+#
+# plot_quality(data, ax, True)
+#
+# plt.show()
