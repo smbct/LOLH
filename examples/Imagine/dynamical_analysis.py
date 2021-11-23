bf452f30f61756e928c37e6a06afcfc7ca1aa18e
smbct <buchet.s@laposte.net>
2021-11-23 17:55:05 +0100

    graphs for the report

--- examples/Imagine/dynamical_analysis.py
+++ examples/Imagine/dynamical_analysis.py
@@ -160,84 +160,181 @@ def plot_gene_umap(df_normalized, df_coordinates, gene, ax):
 
     return
 
-# create_regulatory_graph()
-
-# load the graph from an existing file
-filename = '../../dataset/Imagine/regulatory_network_processed.txt'
-graph = Graph('regulatory network')
-graph.load_from_file(filename)
 
 
-# load the discrete dataset
-filename = '../../dataset/Imagine/discrete_matrix.csv'
-df_discrete = pd.read_csv(filename, index_col = 0)
-print(df_discrete.head())
-
-# load the cell types
-filename = '../../dataset/Imagine/cell_types.csv'
-df_celltypes = pd.read_csv(filename, index_col = 0)
-print(df_celltypes.head())
-
-# load the UMAP representation
-filename = '../../dataset/Imagine/umap_coordinates.csv'
-df_umap = pd.read_csv(filename, index_col = 0)
-print(df_umap.head())
-
-# load the normalized dataset
-filename = '../../dataset/Imagine/normalized_matrix.csv'
-df_normalized = pd.read_csv(filename, index_col = 0)
-df_normalized = df_normalized.T
-print(df_normalized.head())
-
-# display
-print('display the (directed) graph')
-col_option = '01_colors'
-arrows = True
-cluster_size_limit = 20
-fig, ax = plt.subplots()
-graph.plot(ax, col_option, arrows, cluster_size_limit)
-
-col_option = 'clustering_colors'
-fig, ax = plt.subplots()
-graph.plot(ax, col_option, arrows, cluster_size_limit)
+#-------------------------------------------------------------------------------
+def global_analysis():
 
 
-# save the graph
-# filename = '../../dataset/Imagine/regulatory_network_processed.txt'
-# graph.save(filename)
+    # create_regulatory_graph()
 
-clusters = {}
-for cluster in graph.clusters:
-    if len(graph.clusters[cluster]) >= 20:
-        clusters[cluster] = graph.clusters[cluster]
-# print(clusters)
-
-print('plot the clusters')
-# create a fake instance
-instance = Instance.create_random_instance(df_discrete.copy(deep=False), 0.5)
-# cluster_index = list(clusters.keys())[0]
-
-selected_clusters = [6,7,8,9,11,20]
-
-ncol = 2
-nrows = int(len(selected_clusters)/float(ncol))
-fix, axs = plt.subplots(nrows, ncol)
-axs = axs.flat
-ind_plot = 0
-for cluster_index in selected_clusters:
-    ax = axs[ind_plot]
-    body =  [ instance.get_atom_index(graph.atoms[index]) for index in clusters[ cluster_index ] ]
-    # fig, ax = plt.subplots()
-    ax.set_title('cluster ' + str(cluster_index))
-    plot_cluster_umap(df_discrete, df_umap, instance, body, ax)
-    ind_plot += 1
+    # load the graph from an existing file
+    filename = '../../dataset/Imagine/regulatory_network_processed.txt'
+    graph = Graph('regulatory network')
+    graph.load_from_file(filename)
 
 
-# plot a single gene
-gene = 'PPBP'
-fig, ax = plt.subplots()
-ax.set_title('gene ' + gene)
-plot_gene_umap(df_normalized, df_umap, gene, ax)
+    # load the discrete dataset
+    filename = '../../dataset/Imagine/discrete_matrix.csv'
+    df_discrete = pd.read_csv(filename, index_col = 0)
+    print(df_discrete.head())
+
+    # load the cell types
+    filename = '../../dataset/Imagine/cell_types.csv'
+    df_celltypes = pd.read_csv(filename, index_col = 0)
+    print(df_celltypes.head())
+
+    # load the UMAP representation
+    filename = '../../dataset/Imagine/umap_coordinates.csv'
+    df_umap = pd.read_csv(filename, index_col = 0)
+    print(df_umap.head())
+
+    # load the normalized dataset
+    filename = '../../dataset/Imagine/normalized_matrix.csv'
+    df_normalized = pd.read_csv(filename, index_col = 0)
+    df_normalized = df_normalized.T
+    print(df_normalized.head())
+
+    # display
+    print('display the (directed) graph')
+    col_option = '01_colors'
+    arrows = True
+    cluster_size_limit = 20
+    fig, ax = plt.subplots()
+    graph.plot(ax, col_option, arrows, cluster_size_limit)
+
+    col_option = 'clustering_colors'
+    fig, ax = plt.subplots()
+    graph.plot(ax, col_option, arrows, cluster_size_limit)
 
 
-plt.show()
+    ################################################################################
+    #### load and process the coexpression network
+    # input_file = '../../dataset/Imagine/coexpression/coexpression_network.txt'
+    # n_cell_min = 200
+    # score_min = 0.35
+    # louvain_param = 0.7
+    # output_file = '../../dataset/Imagine/coexpression/coexpression_graph.txt'
+    # codata = GData()
+    # codata.load_from_file(input_file, n_cell_min, score_min)
+    # cograph = Graph('coexpression network')
+    # # build graph from raw data, exclude mitochondrial and ribosomal genes
+    # exclude_mt_rp = True
+    # filter_edges = True
+    # cograph.create_from_gdata(codata, exclude_mt_rp, filter_edges)
+    # cograph.compute_clusters(louvain_param)
+    # cograph.compute_positions()
+    # cograph.save(output_file)
+
+    cograph = Graph('coexpression network')
+    cograph.load_from_file('../../dataset/Imagine/coexpression/coexpression_graph.txt')
+    fig, ax = plt.subplots()
+    ax.set_title('coexpression graph')
+    cograph.plot(ax, 'clustering_colors', False, 10)
+
+    # save the dynamical graph
+    # filename = '../../dataset/Imagine/regulatory_network_processed.txt'
+    # graph.save(filename)
+
+    clusters = {}
+    for cluster in graph.clusters:
+        if len(graph.clusters[cluster]) >= 20:
+            clusters[cluster] = graph.clusters[cluster]
+    # print(clusters)
+
+    print('plot the clusters')
+    # create a fake instance
+    instance = Instance.create_random_instance(df_discrete.copy(deep=False), 0.5)
+    # cluster_index = list(clusters.keys())[0]
+
+    selected_clusters = [6,7,8,9,11,20]
+
+    ncol = 2
+    nrows = int(len(selected_clusters)/float(ncol))
+    fix, axs = plt.subplots(nrows, ncol)
+    axs = axs.flat
+    ind_plot = 0
+    for cluster_index in selected_clusters:
+        ax = axs[ind_plot]
+        body =  [ instance.get_atom_index(graph.atoms[index]) for index in clusters[ cluster_index ] ]
+        # fig, ax = plt.subplots()
+        ax.set_title('cluster ' + str(cluster_index))
+        plot_cluster_umap(df_discrete, df_umap, instance, body, ax)
+        ind_plot += 1
+
+
+    # plot a single gene
+    gene = 'PPBP'
+    gene = 'S100A4'
+    gene = 'ANXA1'
+    gene = 'DERL3'
+
+    fig, ax = plt.subplots()
+    ax.set_title('gene ' + gene)
+    plot_gene_umap(df_normalized, df_umap, gene, ax)
+
+    plt.show()
+
+    return
+
+#-------------------------------------------------------------------------------
+def sub_analysis():
+
+    # create the sub dataset limited to the t cell
+    # load the discrete dataset
+    # filename = '../../dataset/Imagine/discrete_matrix.csv'
+    # df_discrete = pd.read_csv(filename, index_col = 0)
+    # # load the cell types
+    # filename = '../../dataset/Imagine/cell_types_macro.csv'
+    # df_celltypes = pd.read_csv(filename, index_col = 0)
+    # # create a sub dataset
+    # T_cells = df_celltypes.loc[df_celltypes['cellType_macro']=='T'].index
+    # sub_dataset = df_discrete.loc[T_cells]
+    # sub_dataset.to_csv('../../dataset/Imagine/discrete_matrix_T_cells.csv')
+
+    # create the subset of transitions limited to the t cells
+    # transitions_df = pd.read_csv('../../dataset/Imagine/transitions.csv', index_col=0)
+    # print(transitions_df.head())
+    # filename = '../../dataset/Imagine/discrete_matrix_T_cells.csv'
+    # df_T = pd.read_csv(filename, index_col = 0)
+    # T_cells = list(df_T.index)
+    # selected_transitions = []
+    # for ind in transitions_df.index:
+    #     if transitions_df['T-1'][ind] in T_cells and transitions_df['T'][ind] in T_cells:
+    #         selected_transitions.append(ind)
+    # transitions_df_sub = transitions_df.loc[selected_transitions]
+    # transitions_df_sub.to_csv('../../dataset/Imagine/transitions_T.csv')
+
+    filename = '../../dataset/Imagine/T_regulatory_network.txt'
+
+    n_cell_min = 20
+    score_min = 0.35
+    louvain_param = 0.7
+    data = GData()
+    data.load_from_file(filename, n_cell_min, score_min)
+    graph = Graph('regulatory network (T cells)')
+    # build graph from raw data, exclude mitochondrial and ribosomal genes
+    exclude_mt_rp = True
+    filter_edges = True
+    graph.create_from_gdata(data, exclude_mt_rp, filter_edges)
+    graph.compute_clusters(louvain_param)
+    graph.compute_positions()
+
+    fig, ax = plt.subplots()
+    ax.set_title('T cels regulatory graph')
+    graph.plot(ax, 'clustering_colors', True, 10)
+
+    fig, ax = plt.subplots()
+    ax.set_title('T cels regulatory graph')
+    graph.plot(ax, '01_colors', True, 10)
+
+    # graph.save(output_file)
+
+    plt.show()
+
+    return
+
+
+# global_analysis()
+
+sub_analysis()
